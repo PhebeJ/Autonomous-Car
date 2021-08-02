0a09be94a24d93a2d3c94487e9c21665baad3945
Phebe Joshua <feba.j7@gmail.com>
2021-08-03 00:19:07 +0530

    Update ghostAgents.py

--- src/ghostAgents.py
+++ src/ghostAgents.py
@@ -1,11 +1,5 @@
 # ghostAgents.py
 # --------------
-# Licensing Information: Please do not distribute or publish solutions to this
-# project. You are free to use and extend these projects for educational
-# purposes. The Car AI projects were developed at UC Berkeley, primarily by
-# John DeNero (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
-# For more info, see http://inst.eecs.berkeley.edu/~cs188/sp09/car.html
-
 from game import Agent
 from game import Actions
 from game import Directions

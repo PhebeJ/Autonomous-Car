f8daf5febc0b155ee26570fbab2bc6cd08ee35f3
Phebe Joshua <feba.j7@gmail.com>
2021-08-03 00:24:36 +0530

    Update mdp.py

--- src/mdp.py
+++ src/mdp.py
@@ -1,13 +1,6 @@
 # mdp.py
 # ------
-# Licensing Information: Please do not distribute or publish solutions to this
-# project. You are free to use and extend these projects for educational
-# purposes. The Car AI projects were developed at UC Berkeley, primarily by
-# John DeNero (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
-# For more info, see http://inst.eecs.berkeley.edu/~cs188/sp09/car.html
-
 import random
-
 class MarkovDecisionProcess:
     
   def getStates(self):

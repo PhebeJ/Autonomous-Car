1bf03756722699d2dd2e24b3555927db5f24a7a7
Phebe Joshua <feba.j7@gmail.com>
2021-08-03 00:19:40 +0530

    Update graphicsCrawlerDisplay.py

--- src/graphicsCrawlerDisplay.py
+++ src/graphicsCrawlerDisplay.py
@@ -1,11 +1,5 @@
 # graphicsCrawlerDisplay.py
 # -------------------------
-# Licensing Information: Please do not distribute or publish solutions to this
-# project. You are free to use and extend these projects for educational
-# purposes. The Car AI projects were developed at UC Berkeley, primarily by
-# John DeNero (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
-# For more info, see http://inst.eecs.berkeley.edu/~cs188/sp09/car.html
-
 import Tkinter
 import qlearningAgents
 import time

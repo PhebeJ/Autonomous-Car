2bd493170d77e19f829aab3a55209955488b5dd0
Phebe Joshua <feba.j7@gmail.com>
2021-08-03 00:16:05 +0530

    Update carAgents.py

--- src/carAgents.py
+++ src/carAgents.py
@@ -1,11 +1,5 @@
 # carAgents.py
 # ---------------
-# Licensing Information: Please do not distribute or publish solutions to this
-# project. You are free to use and extend these projects for educational
-# purposes. The Car AI projects were developed at UC Berkeley, primarily by
-# John DeNero (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
-# For more info, see http://inst.eecs.berkeley.edu/~cs188/sp09/car.html
-
 from car import Directions
 from game import Agent
 import random

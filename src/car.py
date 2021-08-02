487730da20613507f1a942492fe724ca52b0a51d
Phebe Joshua <feba.j7@gmail.com>
2021-08-03 00:15:34 +0530

    Update car.py

--- src/car.py
+++ src/car.py
@@ -1,11 +1,5 @@
 # car.py
 # ---------
-# Licensing Information: Please do not distribute or publish solutions to this
-# project. You are free to use and extend these projects for educational
-# purposes. The Car AI projects were developed at UC Berkeley, primarily by
-# John DeNero (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
-# For more info, see http://inst.eecs.berkeley.edu/~cs188/sp09/car.html
-
 """
 Car.py holds the logic for the classic car game along with the main
 code to run a game.  This file is divided into three sections:
